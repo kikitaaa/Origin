317f81278010b00186ba6ab6ee3e9ae71cc84fed
Karen Fernandez <121123435+kikitaaa@users.noreply.github.com>
2023-03-27 19:27:27 +0200

    Movimiento del menú
    Para que el fondo del menú tenga una pequeña animación al mover de sitio el ratón

---
+++ Origin/Assets/Scripts/MenuMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour
+{
+    float mousePosX;
+    float mousePosY;
+
+    [SerializeField] float movementQuantity;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        mousePosX = Input.mousePosition.x;
+        mousePosY = Input.mousePosition.y;
+
+        this.GetComponent<RectTransform>().position = new Vector2(
+            (mousePosX / Screen.width) * movementQuantity + (Screen.width / 2),
+            (mousePosY / Screen.height) * movementQuantity + (Screen.height / 2));
+
+    }
+}
