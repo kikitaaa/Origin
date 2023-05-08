7080fe6bfffa7c676d9d2663c5229b86cbf2595f
SofiaBittar <121123642+SofiaBittar@users.noreply.github.com>
2023-05-08 17:36:25 +0200

    Barra de vida funcional

---
+++ Origin/Assets/Scripts/healthbar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public Image UIImagen;
+    public GameObject Player;
+    public void Start()
+    {
+        UIImagen = GameObject.Find("Vida").GetComponent<Image>();
+    }
+    public void Update()
+    {
+        UIImagen.sprite = Resources.Load<Sprite>("Barra de vidas/" + Player.GetComponent<Kai>().life.ToString());
+    }
+}
