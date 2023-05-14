e22a816e7d5f8ade0c986f69aa5aad4c63e4d296
Karen Fernandez <121123435+kikitaaa@users.noreply.github.com>
2023-05-14 23:01:46 +0200

    audios+bala

--- Origin/Assets/Scripts/Coins.cs
+++ Origin/Assets/Scripts/Coins.cs
@@ -6,6 +6,9 @@ public class Coins : MonoBehaviour
 {
     public int CoinValue = 1;
     public Shop coins;
+    public AudioClip coinSound;
+    [Range(0, 1)]
+    public float coinVolume;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +18,7 @@ public class Coins : MonoBehaviour
             coins.UpdateCoinsText();
             Debug.Log("Monedas añadidas.");
             Destroy(gameObject);
+            AudioManager.instance.PlayAudio(coinSound, coinVolume);
         }
     }
 }
