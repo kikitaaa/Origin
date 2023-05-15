851497d6ed434da9525f662a71a0635b382e8092
DiegoMoreira98 <diego.moreira098@gmail.com>
2023-05-15 16:38:41 +0200

    cambios varios

--- Origin/Assets/Scripts/Shield.cs
+++ Origin/Assets/Scripts/Shield.cs
@@ -2,35 +2,19 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    public float shieldDuration = 5f; // Duración del escudo en segundos
-    public float shieldStrength = 1f; // Fuerza del escudo (0-1)
-
-    private float shieldTimer;
-
-    private void Start()
+    public string jugadorTag = "Player";
+    public string enemigoTag = "Enemy";
+    void OnTriggerEnter2D(Collider2D other)
     {
-        shieldTimer = shieldDuration;
-    }
-
-    private void Update()
-    {
-        shieldTimer -= Time.deltaTime;
-
-        if (shieldTimer <= 0f)
+        if (other.CompareTag(enemigoTag))
         {
-            Destroy(gameObject); // Destruye el objeto del escudo cuando la duración se agota
+            // Desactivar el comportamiento de ataque del enemigo
+            Luputon2 enemy = other.GetComponent<Luputon2>();
+            if (enemy != null)
+            {
+               // enemy.StopAttack();
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // Calcula el daño reducido por el escudo
-            float reducedDamage = collision.GetComponent<Luputon2>().damage * (1f - shieldStrength);
-
-            // Aplica el daño reducido al enemigo
-            collision.GetComponent<Luputon2>().TakeDamage(reducedDamage);
-        }
-    }
 }
